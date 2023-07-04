3842d372102e0bf500fa2d8ef625fad3ca7f3fdf
mynknight <mayankchoudhary651@gmail.com>
2023-07-04 22:05:01 +0530

    test push

--- FiShare/FiShare/urls.py
+++ FiShare/FiShare/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', users_view.register, name='register'),
     path('profile/', users_view.profile, name='profile'),
+
     path('all_users/', users_view.user_list, name='all_users'),
     path('all_users/<int:user_id>', PostListView.as_view(template_name='root/user_repo.html'),name='user_repo'),
     path('', include('root.urls')),
