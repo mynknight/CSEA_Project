698ace3695dca2fbc91086bce4c706a6c21cb797
mynknight <mayankchoudhary651@gmail.com>
2023-07-04 14:23:21 +0530

    small changes

--- FiShare/root/urls.py
+++ FiShare/root/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns =[
     path('',PostListView.as_view(),name='home'),
-    path('my_files/',PostListView.as_view(),name='all_files'),
+    # path('my_files/',PostListView.as_view(),name='all_files'),
     path('my_files/new/',PostCreateView.as_view(template_name='root/add_file.html'),name='all_files_create'),
     path('my_files/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_file'),
     path('my_files/folder/create', create_folder,name='create_folder'),
